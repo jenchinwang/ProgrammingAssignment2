aa12f9080a8d2ac28a021cc6a9892a5128c0a2b2
jenchinwang <jenchinwang@gmail.com>
2015-07-24 13:56:48 -0400

    Assignment #2 in Coursera R programming course
    Create functions that will return an inverted matrix of the input
    matrix, with a message that let the users know whether this inverted
    matrix is already saved in the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## When below functions are executed in sequence, the user can get an inversed
+## matrix of your input matrix with a message to know if the inversed
+## matrix is already in cache!
+ 
+## The first function will create a list of functions that will give
+## the second function instruction what to do 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_x <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse_x <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(inverse_matrix) inverse_x <<- inverse_matrix
+  getsolve <- function() inverse_x
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The second function will execute the computation to get an 
+## inversed matrix of a given matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_x <- x$getsolve()
+  ## Print a message if data is already in the cache
+  if(!is.null(inverse_x)) {
+    message("getting cached data")
+    return(inverse_x)
+  }
+  data <- x$get()
+  inverse_x <- solve(data, ...)
+  x$setsolve(inverse_x)
+  ## Return a matrix that is the inverse of 'x'
+  inverse_x
 }
